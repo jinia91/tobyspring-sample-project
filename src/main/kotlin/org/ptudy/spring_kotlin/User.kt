66c73b213b2ee913ddfad740bccec6dae8672d3c
jinia91 <creee77@gmail.com>
2024-03-03 14:57:39 +0900

    step 1 user and dao with testcode by main

---
+++ src/main/kotlin/org/ptudy/spring_kotlin/User.kt
@@ -0,0 +1,7 @@
+package org.ptudy.spring_kotlin
+
+data class User(
+    val id: String,
+    var name: String,
+    var password: String
+)
---
+++ src/main/kotlin/org/ptudy/spring_kotlin/UserDao.kt
@@ -0,0 +1,44 @@
+package org.ptudy.spring_kotlin
+
+import java.sql.DriverManager
+
+class UserDao {
+    fun add(user: User) {
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root", "")
+            .use { conn ->
+                conn.prepareStatement("insert into user values(?, ?, ?)").use { stmt ->
+                    stmt.setString(1, user.id)
+                    stmt.setString(2, user.name)
+                    stmt.setString(3, user.password)
+                    stmt.executeUpdate()
+                }
+            }
+    }
+
+    fun get(id: String): User? {
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root", "")
+            .use { conn ->
+                conn.prepareStatement("select * from user where id = ?").use { stmt ->
+                    stmt.setString(1, id)
+                    val rs = stmt.executeQuery()
+                    if (rs.next()) {
+                        return User(
+                            id = rs.getString("id"),
+                            name = rs.getString("name"),
+                            password = rs.getString("password")
+                        )
+                    } else {
+                        return null
+                    }
+                }
+            }
+    }
+}
+
+fun main() {
+    val userDao = UserDao()
+    val user = User("1234", "name", "password")
+    userDao.add(user)
+    val user2 = userDao.get("1234")
+    println(user2)
+}
