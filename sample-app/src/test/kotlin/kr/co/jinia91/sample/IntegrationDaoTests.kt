0da95397cd8a209b51ea52715087cbe86b2df8c4
jinia91 <creee77@gmail.com>
2024-03-16 22:25:35 +0900

    junit singleton test

--- sample-app/src/test/kotlin/kr/co/jinia91/sample/IntegrationDaoTests.kt
+++ sample-app/src/test/kotlin/kr/co/jinia91/sample/IntegrationDaoTests.kt
@@ -2,15 +2,16 @@ package kr.co.jinia91.sample
 
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import kr.co.jinia91.sample.SingletonSpringBootTest.context
 import kr.co.jinia91.sample.application.User
 import kr.co.jinia91.sample.application.UserDao
-import kr.co.jinia91.springframework.core.SpringBootApplication
 import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 
 class IntegrationDaoTests {
 
     private val sut = context.getBean(UserDao::class.java.name) as UserDao
+
     @BeforeEach
     fun setUp() {
         sut.deleteAll()
@@ -31,9 +32,5 @@ class IntegrationDaoTests {
         findUser.shouldNotBeNull()
         findUser.name shouldBe "jinia"
         findUser.password shouldBe "1234"
-    }
-
-    companion object {
-        private var context = SpringBootApplication.run()
     }
 }
---
+++ sample-app/src/test/kotlin/kr/co/jinia91/sample/SingletonSpringBootTest.kt
@@ -0,0 +1,7 @@
+package kr.co.jinia91.sample
+
+import kr.co.jinia91.springframework.core.SpringBootApplication
+
+object SingletonSpringBootTest {
+     val context = SpringBootApplication.run()
+}
