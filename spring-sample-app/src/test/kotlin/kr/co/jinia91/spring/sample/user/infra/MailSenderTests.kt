a5b703c49fd01536e97121ee8dcef2fe941ceece
jinia91 <creee77@gmail.com>
2024-04-13 17:19:50 +0900

    mail service 명세 테스트 케이스 정의

---
+++ spring-sample-app/src/test/kotlin/kr/co/jinia91/spring/sample/user/infra/MailSenderTests.kt
@@ -0,0 +1,13 @@
+package kr.co.jinia91.spring.sample.user.infra
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MailSenderTests {
+    @Autowired
+    private lateinit var sut : MailSender
+    @Test
+    fun `원하는 대상에게 메일을 보내야한다`{}
+}
